d5d5cfa604cb04f32c78817c9cab176dcfe657ef
Florentino Navarro Diaz <tino.navarro@hotmail.com>
2019-10-05 13:33:20 -0500

    NHBoilerplate Done

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import {hola} from "./modules/example"
+// import {activeMenu} from './modules/active-menu'
+
+// saludo()
+// despedida()
+// activeMenu()
+hola()
+console.log('hola mundo cuatro jhhk')
+console.log('otra cosa')
+console.log('otra')
